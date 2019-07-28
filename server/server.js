18627532b816e1efdba987888313b77a1280a376
AniketLivan <kumaraniket383@gmail.com>
2019-07-29 00:11:18 +0530

    added update for the patch route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
- 
+const _id = require('lodash');
+
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -31,7 +32,7 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
-
+//to get the id and see its key values
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if(!ObjectID.isValid(id)){
@@ -43,6 +44,48 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
         }
         res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+//to remove the existing data of a specific user
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+        return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) =>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, {$set:body},{new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
