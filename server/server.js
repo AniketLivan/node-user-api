a500e8de79927ee2b6d1f20174cc7629f913f4db
AniketLivan <kumaraniket383@gmail.com>
2019-07-28 20:40:48 +0530

    New ways for getting specific id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
  
 
 var {mongoose} = require('./db/mongoose');
@@ -30,8 +31,24 @@ app.get('/todos', (req, res) => {
     })
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+        return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
-})
+});
 
 module.exports = {app};
