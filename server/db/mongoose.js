e3f87d4a76e5b8c9845463d6b84da4357a4dd5a4
AniketLivan <kumaraniket383@gmail.com>
2019-07-28 22:08:15 +0530

    Dont know whats gonna happen

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -48,7 +49,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Started on port 3000');
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
